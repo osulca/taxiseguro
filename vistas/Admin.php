c3be2792d2e8279655e9789ad75126b11c49d642
joaquin orihuela <axxdderr@gmail.com>
2019-06-26 12:41:04 -0500

    Ventana para administradores

---
+++ vistas/Admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Administradores</title>
+    </head>
+    <body>
+        <form action="#" method="get">
+            <text>$nombreAdm
+        <button>Registrar Vehiculos</button>
+        <button>Mostrar Registros</button>
+        </form>
+        <?php
+        include '../bd/Conexion.php';
+        ?>
+    </body>
+</html>
